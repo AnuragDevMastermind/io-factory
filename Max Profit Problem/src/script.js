4cb027d3dd12ccd0c82fea19f177cf254bce34d2
Anurag Sagar <anurag983sagarofficial@gmail.com>
2024-05-14 08:15:59 +0530

    iteration

--- Max Profit Problem/src/script.js
+++ Max Profit Problem/src/script.js
@@ -1,101 +1,142 @@
-// Constants
-const timeUnit = 49
-const theaterBuildTime = 5
-const pubBuildTime = 4
-const commercialParkBuildTime = 10
-const theaterEarning = 1500
-const pubEarning = 1000
-const commercialParkEarning = 3000
+const properties = [
+  { name: "Commercial Park", shortName: "C", buildTime: 10, earning: 3000 },
+  { name: "Pub", shortName: "P", buildTime: 4, earning: 1000 },
+  { name: "Theater", shortName: "T", buildTime: 5, earning: 1500 },
+]
+const totalUnit = 7
 
-// Main
-const earningCombinations = generateEarningCombinations()
-printMaxEarnings(earningCombinations)
+const combination = []
 
-// Find maximum possible combinations and their earnings
-function generateEarningCombinations() {
-  const maxTheater = Math.floor(timeUnit / theaterBuildTime)
-  const maxPub = Math.floor(timeUnit / pubBuildTime)
-  const maxCommercial = Math.floor(timeUnit / commercialParkBuildTime)
+sortProperties(properties, totalUnit)
+generateCombinations(properties, totalUnit)
+printMaxEarnings(combination, properties, totalUnit)
 
-  const combinations = []
 
-  for (let theaterQty = 0; theaterQty <= maxTheater; theaterQty++) {
-    for (let pubQty = 0; pubQty <= maxPub; pubQty++) {
-      for (
-        let commercialParkQty = 0;
-        commercialParkQty <= maxCommercial;
-        commercialParkQty++
-      ) {
-        const totalBuildTime =
-          theaterBuildTime * theaterQty +
-          pubBuildTime * pubQty +
-          commercialParkBuildTime * commercialParkQty
-        if (totalBuildTime < timeUnit) {
-          const earnings = calculateEarnings(
-            commercialParkQty,
-            theaterQty,
-            pubQty,
-            timeUnit
-          )
-          combinations.push({
-            E: earnings,
-            C: commercialParkQty,
-            T: theaterQty,
-            P: pubQty,
-          })
+/**
+ * Sorts the properties based on their earning per unit time.
+ * @param {Array} properties Array of property objects.
+ * @param {number} totalUnit Total time units available.
+ */
+function sortProperties(properties, totalUnit) {
+  properties.forEach((property) => {
+    property.maxCount = Math.floor(totalUnit / property.buildTime)
+  })
+
+  properties.sort((a, b) => {
+    const ratioA = a.earning / a.buildTime
+    const ratioB = b.earning / b.buildTime
+    return ratioB - ratioA
+  })
+}
+
+/**
+ * Generates all possible combinations of properties.
+ * @param {Array} properties Array of property objects.
+ */
+function generateCombinations(properties) {
+  const buildTimes = properties.map(property => Math.floor(totalUnit / property.buildTime));
+
+  generateCombinationsHelper(buildTimes, buildTimes)
+}
+
+/**
+ * Helper function to generate combinations recursively.
+ * @param {Array} pArr Array representing current combination.
+ * @param {Array} upArr Array representing maximum units for each property.
+ */
+function generateCombinationsHelper(pArr, upArr) {
+
+  if (pArr.every((val) => val === 0)) return
+
+  if (checkValidCombination(pArr, properties, totalUnit)) {
+    const totalEarning = calculateEarningsForCombination(
+      pArr,
+      properties,
+      totalUnit
+    )
+    combination.push(totalEarning)
+  }
+
+  let newPArr = pArr.slice()
+  const n = pArr.length 
+
+  for (let i = n - 1; i >= 0; i--) {
+    if (pArr[i] > 0) {
+      newPArr[i]--
+      generateCombinationsHelper(newPArr, upArr)
+      break 
+    }
+    else {
+      newPArr[i] = upArr[i]
+      
+      if (i > 0) continue
+      
+      if (newPArr[i - 1] > 0) {
+        newPArr[i - 1]--
+        for (let j = i; j < n; j++) {
+          newPArr[j] = upArr[j]
         }
+        generateCombinationsHelper(newPArr, upArr)
       }
     }
   }
-
-  return combinations
 }
 
-// Function to calculate earnings
-function calculateEarnings(comParkCount, theatreCount, pubCount, totalTime) {
+/**
+ * Checks if a combination is valid based on total time units.
+ * @param {Array} temp Array representing the current combination.
+ * @param {Array} properties Array of property objects.
+ * @param {number} timeUnit Total time units available.
+ * @returns {boolean} True if combination is valid, otherwise false.
+ */
+function checkValidCombination(temp, properties, timeUnit) {
+  let sum = 0
+  for (let j = 0; j < temp.length; j++) {
+    sum += temp[j] * properties[j].buildTime
+  }
+  return sum < timeUnit
+}
+
+/**
+ * Calculates total earning for a combination.
+ * @param {Array} combination Array representing a combination.
+ * @param {Array} properties Array of property objects.
+ * @param {number} totalTime Total time units available.
+ * @returns {Object} Object containing total earning and combination.
+ */
+function calculateEarningsForCombination(combination, properties, totalTime) {
   let totalEarning = 0
-
-  // Commercial park earnings
-  for (let i = 1; i <= comParkCount; i++) {
-    totalEarning +=
-      commercialParkEarning * (totalTime - commercialParkBuildTime * i)
-  }
-
-  // Theater earnings
-  for (let i = 1; i <= theatreCount; i++) {
-    totalEarning +=
-      theaterEarning *
-      (totalTime -
-        (commercialParkBuildTime * comParkCount + theaterBuildTime * i))
-  }
-
-  // Pub earnings
-  for (let i = 1; i <= pubCount; i++) {
-    totalEarning +=
-      pubEarning *
-      (totalTime -
-        (commercialParkBuildTime * comParkCount +
-          theaterBuildTime * theatreCount +
-          pubBuildTime * i))
-  }
-
-  return totalEarning
-}
-
-// Print combinations with maximum earnings
-function printMaxEarnings(combinations) {
-  combinations.sort((a, b) => b.E - a.E)
-  const maxEarning = combinations[0].E
-  console.log(`Time Unit: ${timeUnit}`)
-  console.log(`Earnings: $${maxEarning}\nSolutions`)
-
-  for (const [index, combination] of combinations.entries()) {
-    if (combination.E === maxEarning) {
-      console.log(
-        `   ${index + 1}. T:${combination.T} P:${combination.P} C:${combination.C} `
-      )
-    } else {
-      break
+  let remainingTime = totalTime
+  for (let j = 0; j < combination.length; j++) {
+    for (let k = 1; k <= combination[j]; k++) {
+      remainingTime -= properties[j].buildTime
+      totalEarning += properties[j].earning * remainingTime
     }
   }
+  return { earning: totalEarning, combination }
+}
+
+/**
+ * Prints maximum earnings and corresponding solutions.
+ * @param {Array} earnings Array of earnings objects.
+ * @param {Array} properties Array of property objects.
+ * @param {number} timeUnit Total time units available.
+ */
+function printMaxEarnings(earnings, properties, timeUnit) {
+  const sortedEarnings = earnings.sort((a, b) => b.earning - a.earning)
+  const maxEarning = sortedEarnings[0].earning
+  console.log(`Time Unit: ${timeUnit}`)
+  console.log(`Earnings: $${maxEarning}\nSolutions`)
+  sortedEarnings.forEach((earning, index) => {
+    if (earning.earning === maxEarning) {
+      let str = "  "
+      str += `${index + 1}. `
+      for (let i = 0; i < earning.combination.length; i++) {
+        str += `${properties[i].shortName}:${earning.combination[i]} `
+      }
+      console.log(str)
+    } else {
+      return
+    }
+  })
 }
--- Water Tank Problem/src/script.js
+++ Water Tank Problem/src/script.js
@@ -1,17 +1,17 @@
-let txtInput;
+let txtInput
 
 function checkValidation() {
-  txtInput = document.getElementById("txtInput").value;
-  const txtValidationMsgElement = document.getElementById("txtValidation");
-  const btnSubmitElement = document.getElementById("btnSubmit");
+  txtInput = document.getElementById("txtInput").value
+  const txtValidationMsgElement = document.getElementById("txtValidation")
+  const btnSubmitElement = document.getElementById("btnSubmit")
 
   // all input validation condition
-  const isNotNum = /[^0-9,]/.test(txtInput); // input must be number validation
-  const isconsecutiveComma = /,,/.test(txtInput); // no consecutive comma validation
-  const isCommaAtStart = txtInput.charAt(0) === ","; // no comma at start validation
-  const isCommaAtEnd = txtInput.charAt(txtInput.length - 1) === ","; // no comma at end validation
-  const isAllInputZero = txtInput.split(",").every((value) => value === "0"); // all input not equal zero validation
-  const isEmpty = txtInput.trim() === ""; // input not empty validation
+  const isNotNum = /[^0-9,]/.test(txtInput) // input must be number validation
+  const isconsecutiveComma = /,,/.test(txtInput) // no consecutive comma validation
+  const isCommaAtStart = txtInput.charAt(0) === "," // no comma at start validation
+  const isCommaAtEnd = txtInput.charAt(txtInput.length - 1) === "," // no comma at end validation
+  const isAllInputZero = txtInput.split(",").every((value) => value === "0") // all input not equal zero validation
+  const isEmpty = txtInput.trim() === "" // input not empty validation
 
   const isValidInput =
     isNotNum ||
@@ -34,117 +34,112 @@ function checkValidation() {
   }
 }
 
-function btnSubmitClick() {
-  const inputNumArr = txtInput.split(",").map(Number);
-  const requiredData = calculateRequiredData(inputNumArr);
-  const table2dArr = requiredData[0];
-  const totalWaterUnits = requiredData[1];
+function setValidationMessage(msgElement, message) {
+  const btnSubmitElement = document.getElementById("btnSubmit")
+  msgElement.textContent = message
+  msgElement.style.color = message === "valid" ? "black" : "red"
+  btnSubmitElement.disabled = message != "valid"
+}
 
-  showGraph(table2dArr, txtInput, totalWaterUnits);
+function btnSubmitClick() {
+  const inputNumArr = txtInput.split(",").map(Number)
+  const requiredData = calculateRequiredData(inputNumArr)
+  const table2dArr = requiredData[0]
+  const totalWaterUnits = requiredData[1]
+
+  showGraph(table2dArr, txtInput, totalWaterUnits)
 }
 
 function showGraph(table2dArr, inputData, outputData) {
-  const inputAndOutputTableHtmlArr = createInputAndOutputTableHtml(table2dArr);
-  const inputTableHtml = inputAndOutputTableHtmlArr[0];
-  const outputTableHtml = inputAndOutputTableHtmlArr[1];
+  const inputAndOutputTableHtmlArr = createInputAndOutputTableHtml(table2dArr)
+  const inputTableHtml = inputAndOutputTableHtmlArr[0]
+  const outputTableHtml = inputAndOutputTableHtmlArr[1]
 
-  let resultElement = document.getElementById("result");
-  resultElement.innerHTML = `<p style="font-size: 15px">Input</p>`;
-  resultElement.innerHTML += `<p style="font-size: 15px">[${inputData}]</p>`;
-  resultElement.innerHTML += inputTableHtml;
-  resultElement.innerHTML += `<p style="font-size: 15px">Output</p>`;
-  resultElement.innerHTML += `<p style="font-size: 15px">${outputData} units</p>`;
-  resultElement.innerHTML += outputTableHtml;
+  let resultElement = document.getElementById("result")
+  resultElement.innerHTML = `<p style="font-size: 15px">Input</p>`
+  resultElement.innerHTML += `<p style="font-size: 15px">[${inputData}]</p>`
+  resultElement.innerHTML += inputTableHtml
+  resultElement.innerHTML += `<p style="font-size: 15px">Output</p>`
+  resultElement.innerHTML += `<p style="font-size: 15px">${outputData} units</p>`
+  resultElement.innerHTML += outputTableHtml
 }
 
 function createInputAndOutputTableHtml(table2dArr) {
-  let inputTableHTML = "<table>";
-  let outputTableHTML = "<table>";
+  let inputTableHTML = "<table>"
+  let outputTableHTML = "<table>"
 
   // adding blank row at the top
-  inputTableHTML += "<tr>";
-  outputTableHTML += "<tr>";
+  inputTableHTML += "<tr>"
+  outputTableHTML += "<tr>"
   for (let column = 0; column < table2dArr[0].length; column++) {
-    inputTableHTML += `<td></td>`;
-    outputTableHTML += `<td></td>`;
+    inputTableHTML += `<td></td>`
+    outputTableHTML += `<td></td>`
   }
-  inputTableHTML += "</tr>";
-  outputTableHTML += "</tr>";
+  inputTableHTML += "</tr>"
+  outputTableHTML += "</tr>"
 
   // adding colors
   for (let row = 0; row < table2dArr.length; row++) {
-    inputTableHTML += "<tr>";
-    outputTableHTML += "<tr>";
+    inputTableHTML += "<tr>"
+    outputTableHTML += "<tr>"
 
     for (let column = 0; column < table2dArr[row].length; column++) {
       if (table2dArr[row][column] === 0) {
-        inputTableHTML += `<td></td>`;
-        outputTableHTML += `<td></td>`;
+        inputTableHTML += `<td></td>`
+        outputTableHTML += `<td></td>`
       } else if (table2dArr[row][column] === 1) {
-        inputTableHTML += `<td style="background-color: yellow"></td>`;
-        outputTableHTML += `<td></td>`;
+        inputTableHTML += `<td style="background-color: yellow"></td>`
+        outputTableHTML += `<td></td>`
       } else {
-        inputTableHTML += `<td style="background-color: skyblue"></td>`;
-        outputTableHTML += `<td style="background-color: skyblue"></td>`;
+        inputTableHTML += `<td style="background-color: skyblue"></td>`
+        outputTableHTML += `<td style="background-color: skyblue"></td>`
       }
     }
 
-    inputTableHTML += "</tr>";
-    outputTableHTML += "</tr>";
+    inputTableHTML += "</tr>"
+    outputTableHTML += "</tr>"
   }
 
-  inputTableHTML += "</table>";
-  outputTableHTML += "</table>";
+  inputTableHTML += "</table>"
+  outputTableHTML += "</table>"
 
-  return [inputTableHTML, outputTableHTML];
+  return [inputTableHTML, outputTableHTML]
 }
 
 function calculateRequiredData(height) {
-  const n = height.length;
-  const leftMax = new Array(n).fill(0);
-  const rightMax = new Array(n).fill(0);
 
-  leftMax[0] = 0;
-  for (let i = 1; i < n; ++i) {
-    leftMax[i] = Math.max(height[i - 1], leftMax[i - 1]);
-  }
+  const maxWaterLevel = Math.max(...height)
 
-  rightMax[n - 1] = 0;
-  for (let i = n - 2; i >= 0; --i) {
-    rightMax[i] = Math.max(height[i + 1], rightMax[i + 1]);
-  }
+  const n = height.length
 
-  let waterBlocks = 0;
-  const waterTrappedArr = new Array(n).fill(0);
+  let waterBlocks = 0
+  const waterTrappedArr = new Array(n).fill(0)
   for (let i = 0; i < n; ++i) {
-    const waterLevel = Math.min(leftMax[i], rightMax[i]);
-    if (waterLevel >= height[i]) {
-      waterTrappedArr[i] = waterLevel - height[i];
-      waterBlocks += waterTrappedArr[i];
-    }
+    waterTrappedArr[i] = maxWaterLevel - height[i]
+    waterBlocks += waterTrappedArr[i]
   }
 
-  const maxHeight = Math.max(...height);
+  const maxHeight = Math.max(...height)
   const table2dArr = Array.from({ length: maxHeight }, () =>
     Array.from({ length: height.length }, () => 0)
-  );
+  )
 
-  let blockHeight;
-  let waterHeight;
+  let blockHeight
+  let waterHeight
   for (let col = 0; col < height.length; col++) {
-    blockHeight = height[col];
-    waterHeight = waterTrappedArr[col];
+    blockHeight = height[col]
+    waterHeight = waterTrappedArr[col]
 
     for (let row = maxHeight - 1; row >= 0; row--) {
       if (blockHeight > 0) {
-        table2dArr[row][col] = 1;
-        blockHeight--;
+        table2dArr[row][col] = 1
+        blockHeight--
       } else if (waterHeight > 0) {
-        table2dArr[row][col] = 2;
-        waterHeight--;
+        table2dArr[row][col] = 2
+        waterHeight--
       }
     }
   }
 
-  return [table2dArr, waterBlocks];
+  return [table2dArr, waterBlocks]
 }
